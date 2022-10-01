82c7c6c350f9790b4dc64923dc28702c44c82b5f
Matthias Hölzl <tc@xantira.com>
2022-10-01 16:39:25 +0200

    Switch from typer to click

--- {{cookiecutter.project_dir}}/src/{{cookiecutter.project_package}}/__main__.py
+++ {{cookiecutter.project_dir}}/src/{{cookiecutter.project_package}}/__main__.py
@@ -1,12 +1,15 @@
-import typer
-
-app = typer.Typer()
+import click
 
 
-@app.command()
 def say_hi(name="world"):
     print(f"Hello, {name}!")
 
 
+@click.command()
+@click.option("--name", help="The name of the person to greet.", default="world")
+def main(name):
+    print(f"Hello, {name}!")
+
+
 if __name__ == "__main__":
-    app()
+    main()
